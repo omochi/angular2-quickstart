fa8b71330aa78c5762233d66e7e900e40a291666
omochimetaru <omochi.metaru@gmail.com>
2016-04-18 13:04:24 +0900

    why can it compile

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,32 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
+class Cat {
+	name: string;
+	age: number;
+
+	constructor(name: string) {
+		this.name = name;
+	}
+
+	greet(): void {
+		console.log("I am " + this.name);
+	}
+}
+
+var cat1 = new Cat("tama");
+cat1.greet();
+
+var cat2 = {
+	name: "mike",
+	age: 3
+};
+cat2.greet();
+
+var cat3 = {
+	namae: "hoge",
+	hage: 4
+};
+cat3.greet();
+
 bootstrap(AppComponent);
